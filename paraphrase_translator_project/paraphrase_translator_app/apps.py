55d839d0c6ca1b769046d656516aeda078ebc4c7
Danesh <138777868+Danesh1520@users.noreply.github.com>
2023-12-21 11:29:05 +0530

    Add files via upload

---
+++ paraphrase_translator_project/paraphrase_translator_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ParaphraseTranslatorAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'paraphrase_translator_app'
