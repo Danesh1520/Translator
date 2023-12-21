82830a186a3740f968d4c74f222765e74e957f0f
Danesh <138777868+Danesh1520@users.noreply.github.com>
2023-12-21 18:35:53 +0530

    Update settings.py

--- paraphrase_translator_project/paraphrase_translator_project/settings.py
+++ paraphrase_translator_project/paraphrase_translator_project/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMP_DIR = ../paraphrase_translator_app/templates
+TEMP_DIR = '../paraphrase_translator_app/templates'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
