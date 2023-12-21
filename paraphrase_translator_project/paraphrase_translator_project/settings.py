5519198fe5b617e64882dcdf2129b5b5c9212763
Danesh <138777868+Danesh1520@users.noreply.github.com>
2023-12-21 11:34:33 +0530

    Update settings.py

--- paraphrase_translator_project/paraphrase_translator_project/settings.py
+++ paraphrase_translator_project/paraphrase_translator_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#$5c%w@sg@&!1%696j9rs=2kj_lvk-tepo9re%cb*tf3&qbyp0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
